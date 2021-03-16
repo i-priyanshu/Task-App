f27a2ad0e60d528032cdaae2eccf917762ac0381
Priyanshu Kumar <i.priyanshukumar01@gmail.com>
2021-03-16 13:45:35 +0530

    Uploaded Server (MongoDB)

---
+++ mongodb.js
@@ -0,0 +1,22 @@
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
+
+const connectionURL = "mongodb://127.0.0.1:27017";
+const databaseName = "task-manager";
+
+MongoClient.connect(
+  connectionURL,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (error, client) => {
+    if (error) {
+      return console.log("Unable to connect to database");
+    }
+
+    const db = client.db(databaseName);
+
+    db.collection("users").insertOne({
+      name: "Priyanshu",
+      age: 19,
+    });
+  }
+);
