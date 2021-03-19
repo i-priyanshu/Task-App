6c6c292c3daec4777a48b4734b28ab9a28d7d643
Priyanshu Kumar <i.priyanshukumar01@gmail.com>
2021-03-19 19:24:35 +0530

    ID ops.

--- mongodb.js
+++ mongodb.js
@@ -1,8 +1,14 @@
-const mongodb = require("mongodb");
-const MongoClient = mongodb.MongoClient;
+// const mongodb = require("mongodb");
+// const MongoClient = mongodb.MongoClient;
+// const ObjectId = mongodb.ObjectID
+
+const { MongoClient, ObjectID } = require("mongodb");
 
 const connectionURL = "mongodb://127.0.0.1:27017";
 const databaseName = "task-manager";
+
+const id = new ObjectID();
+console.log(id);
 
 MongoClient.connect(
   connectionURL,
@@ -14,9 +20,59 @@ MongoClient.connect(
 
     const db = client.db(databaseName);
 
-    db.collection("users").insertOne({
-      name: "Priyanshu",
-      age: 19,
-    });
+    // db.collection("users").insertOne(
+    //   {
+    //     name: "Priyanshu",
+    //     age: 19,
+    //   },
+    //   (error, result) => {
+    //     if (error) {
+    //       return console.log("Unable to insert User");
+    //     }
+    //     console.log(result.ops);
+    //   }
+    // );
+    //   db.collection("users").insertMany(
+    //     [
+    //       {
+    //         name: "Jan",
+    //         age: 28,
+    //       },
+    //       {
+    //         name: "Gunther",
+    //         age: 27,
+    //       },
+    //     ],
+    //     (error, result) => {
+    //       if (error) {
+    //         return console.log("Unable to insert documents!");
+    //       }
+    //       console.log(result.ops);
+    //     }
+    //   );
+    // }
+
+    // db.collection("users").insertMany(
+    //   [
+    //     {
+    //       description: "blah blah blah!! 1 ",
+    //       completed: true,
+    //     },
+    //     {
+    //       description: "blah blah blah 2",
+    //       completed: true,
+    //     },
+    //     {
+    //       description: "blah blah blah 3",
+    //       completed: false,
+    //     },
+    //   ],
+    //   (error, result) => {
+    //     if (error) {
+    //       return console.log("Unable to  insert documents ");
+    //     }
+    //     console.log(result.ops);
+    //   }
+    // );
   }
 );
