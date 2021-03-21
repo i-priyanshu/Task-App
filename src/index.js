9be2e427fff9a3f2570827e77f56d923f6d739c2
Priyanshu Kumar <i.priyanshukumar01@gmail.com>
2021-03-21 23:56:51 +0530

    Reading Endpoints

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 require("./db/mongoose");
 const User = require("./models/user");
+const Task = require("./models/task");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -20,6 +21,64 @@ app.post("/users", (req, res) => {
     });
 });
 
+app.get("/users/:id", (req, res) => {
+  const _id = req.params.id;
+  User.findById(_id)
+    .then((user) => {
+      if (!user) {
+        return res.status(404).send();
+      }
+      res.send(user);
+    })
+    .catch((e) => {
+      res.send(500).send();
+    });
+});
+
+app.get("/users", (req, res) => {
+  User.find({})
+    .then((users) => {
+      res.send(users);
+    })
+    .catch((e) => {
+      res.status(500);
+    });
+});
+
+app.post("/tasks", (req, res) => {
+  const task = new Task(req.body);
+  task
+    .save()
+    .then(() => {
+      res.send(task);
+    })
+    .catch((e) => {
+      res.status(400);
+      res.send(e);
+    });
+});
+
+app.get("/tasks", (req, res) => {
+  Task.find({})
+    .then((tasks) => {
+      res.send(tasks);
+    })
+    .catch((e) => {
+      res.status(500);
+    });
+});
+
+app.get("/tasks/:id", (req, res) => {
+  const _id = req.params.id;
+  Task.findById(_id)
+    .then((task) => {
+      res.send(task);
+    })
+    .catch((e) => {
+      res.status(400);
+      res.send(e);
+    });
+});
 app.listen(port, () => {
   console.log("server is on port " + port);
 });
