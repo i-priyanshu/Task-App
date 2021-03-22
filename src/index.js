1c96304be455ddd19592826c12d0ef6f7edb34c5
Priyanshu Kumar <i.priyanshukumar01@gmail.com>
2021-03-22 21:47:17 +0530

    Deleting Endpoint

--- src/index.js
+++ src/index.js
@@ -66,6 +66,18 @@ app.patch("/users/:id", async (req, res) => {
   }
 });
 
+app.delete("/users/:id", async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) {
+      res.status(404).send();
+    }
+    res.send(user);
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+
 app.post("/tasks", async (req, res) => {
   const task = new Task(req.body);
   try {
@@ -90,13 +102,49 @@ app.get("/tasks/:id", async (req, res) => {
   try {
     const task = await Task.findById(_id);
     if (!task) {
-      res.status(404).send(task);
+      return res.status(404).send(task);
     }
     res.send(task);
   } catch (e) {
     res.status(500).send();
   }
 });
+
+app.patch("/tasks/:id", async (req, res) => {
+  const updates = Object.keys(req.body);
+  const allowedUpdates = ["description", "completed"];
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update)
+  );
+  try {
+    const task = await new Task.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!isValidOperation) {
+      return res.status(400).send({ error: "Invalid Response" });
+    }
+    if (!task) {
+      res.status(404).send();
+    }
+    res.send(task);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
+
+app.delete("/tasks/:id", async (req, res) => {
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id);
+    if (!task) {
+      res.status(404).send();
+    }
+    res.send(task);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+
 app.listen(port, () => {
   console.log("server is on port " + port);
 });
