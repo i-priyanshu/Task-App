68539bf46ecdbc8d28a24ad52c327db623708ee7
Priyanshu Kumar <i.priyanshukumar01@gmail.com>
2021-03-27 21:24:05 +0530

    Hiding Data

--- src/models/user.js
+++ src/models/user.js
@@ -51,6 +51,14 @@ const userSchema = new mongoose.Schema({
   ],
 });
 
+userSchema.methods.toJSON = function () {
+  const user = this;
+  const userObject = user.toObject();
+  delete userObject.password;
+  delete userObject.tokens;
+  return userObject;
+};
+
 userSchema.methods.generateAuthToken = async function () {
   const user = this;
   const token = jwt.sign({ _id: user._id.toString() }, "iampriyanshu");
