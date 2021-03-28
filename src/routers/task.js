405e25750197f2073226c005d72d9b5d60d4b7c8
Priyanshu Kumar <i.priyanshukumar01@gmail.com>
2021-03-28 21:17:15 +0530

    sorting Data

--- src/routers/task.js
+++ src/routers/task.js
@@ -20,12 +20,18 @@ router.post("/tasks", auth, async (req, res) => {
 
 //completed=true or =False
 //limit=10&skip=2
-//GET /tasks?sortBy=createdAt_desc
+//GET /tasks?sortBy=createdAt:desc
 router.get("/tasks", auth, async (req, res) => {
   const match = {};
+  const sort = {};
 
   if (req.query.completed) {
     match.completed = req.query.completed === "true";
+  }
+
+  if (req.query.sortBy) {
+    const parts = req.query.sortBy.split(":");
+    sort[parts[0]] = parts[1] === "desc" ? -1 : 1;
   }
 
   try {
@@ -36,6 +42,7 @@ router.get("/tasks", auth, async (req, res) => {
         options: {
           limit: parseInt(req.query.limit),
           skip: parseInt(req.query.skip),
+          sort,
         },
       })
       .execPopulate();
