9120decd11c4119bbeec49082907abebf1029da4
Priyanshu Kumar <i.priyanshukumar01@gmail.com>
2021-03-29 11:19:00 +0530

    Handling Errors

--- src/routers/user.js
+++ src/routers/user.js
@@ -104,15 +104,22 @@ const upload = multer({
     fileSize: 2000000,
   },
   fileFilter(req, file, cb) {
-    if (file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
       return cb(new Error("File must be an image"));
     }
     cb(undefined, true);
   },
 });
 
-router.post("/users/me/avatar", upload.single("avatar"), (req, res) => {
-  res.send();
-});
+router.post(
+  "/users/me/avatar",
+  upload.single("avatar"),
+  (req, res) => {
+    res.send();
+  },
+  (error, req, res, next) => {
+    res.status(400).send({ error: error.message });
+  }
+);
 
 module.exports = router;
