49fbb50294cb533dddcb6ffb2547dbff333b5883
Priyanshu Kumar <i.priyanshukumar01@gmail.com>
2021-03-29 10:16:37 +0530

    File Uploads

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = new express.Router();
+const multer = require("multer");
 const User = require("../models/user");
 const auth = require("../middleware/auth");
 
@@ -95,6 +96,14 @@ router.delete("/users/me", auth, async (req, res) => {
   } catch (e) {
     res.status(500).send();
   }
+});
+
+const upload = multer({
+  dest: "avatars",
+});
+
+router.post("/users/me/avatar", upload.single("avatar"), (req, res) => {
+  res.send();
 });
 
 module.exports = router;
