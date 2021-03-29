995f817f077c848be426b071ed0996629ea4880d
Priyanshu Kumar <i.priyanshukumar01@gmail.com>
2021-03-29 11:01:21 +0530

    validating File uploads

--- src/routers/user.js
+++ src/routers/user.js
@@ -100,6 +100,15 @@ router.delete("/users/me", auth, async (req, res) => {
 
 const upload = multer({
   dest: "avatars",
+  limits: {
+    fileSize: 2000000,
+  },
+  fileFilter(req, file, cb) {
+    if (file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+      return cb(new Error("File must be an image"));
+    }
+    cb(undefined, true);
+  },
 });
 
 router.post("/users/me/avatar", upload.single("avatar"), (req, res) => {
